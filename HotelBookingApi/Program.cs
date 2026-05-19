6fa1ae764e0f5bd798c671fb2aff58627defbf7e
fataleffect <dwayne.valdelievre@gmail.com>
2026-05-19 17:33:19 +0100

    Added swagger

--- HotelBookingApi/Program.cs
+++ HotelBookingApi/Program.cs
@@ -1,5 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "HotelBookingApi";
+    config.Title = "HotelBookingApi v1";
+    config.Version = "v1";
+});
+
 var app = builder.Build();
+
+app.UseOpenApi();
+app.UseSwaggerUi(config =>
+{
+    config.DocumentTitle = "TodoAPI";
+    config.Path = "/swagger";
+    config.DocumentPath = "/swagger/{documentName}/swagger.json";
+    config.DocExpansion = "list";
+});
 
 app.MapGet("/", () => "Hello World!");
 
